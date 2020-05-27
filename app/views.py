232a12634806ee1806188fe461681d21a253e9c0
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-05-27 11:21:30 +0300

    add changes to views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_movies
+from .requests import get_movies,get_movie
 
 # Views
 @app.route('/')
@@ -25,3 +26,14 @@ def movie(movie_id):
     '''
     title = f'You are viewing {movie_id}'
     return render_template('movie.html',title = title)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)    
