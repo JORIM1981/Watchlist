64b129b902204e8936672190673e0cc65739ea9d
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-03 16:33:37 +0300

    add photos folder

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://midumbi:midumbi@localhost/watch_list'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos' 
 
 
 
