564f500c3ae7686b04b1da27ed7f55819f6fa744
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-03 18:37:01 +0300

    add upload image and sending email

--- config.py
+++ config.py
@@ -11,25 +11,27 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos' 
 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'midumbi.webdev@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+    
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
 
     DEBUG = True
 
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY=edca5ff751af29035cfd79a9609857c0
 export SECRET_KEY=JORIM1981
+export MAIL_USERNAME=midumbi.webdev@gmail.com
+export MAIL_PASSWORD=moringa81
 
 python3 manage.py server
